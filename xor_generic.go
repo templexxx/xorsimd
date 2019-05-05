c78b793bae46f7825f8216abbeddad2ea9adebcf
templexxx <lucas1x1x@gmail.com>
2019-05-05 22:31:35 +0800

    fix update bug (!amd64)

--- xor_generic.go
+++ xor_generic.go
@@ -1,3 +1,7 @@
+// Copyright 2013 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 // +build !amd64
 
 package xor
@@ -7,108 +11,70 @@ import (
 	"unsafe"
 )
 
-// split slice for cache-friendly
-const unitSize = 16 * 1024
-
 func encode(dst []byte, src [][]byte, feature int) {
-	size := len(dst)
-	start := 0
-	do := unitSize
-	for start < size {
-		end := start + do
-		if end <= size {
-			partNoSIMD(start, end, dst, src)
-			start = start + do
-		} else {
-			partNoSIMD(start, size, dst, src)
-			start = size
-		}
-	}
-}
-
-// split vect will improve performance with big data by reducing cache pollution
-func partNoSIMD(start, end int, dst []byte, src [][]byte) {
-	bytesNoSIMD(dst[start:end], src[0][start:end], src[1][start:end], end-start)
-	for i := 2; i < len(src); i++ {
-		bytesNoSIMD(dst[start:end], dst[start:end], src[i][start:end], end-start)
-	}
-}
-
-// fastXORBytes xor in bulk. It only works on architectures that
-// support unaligned read/writes.
-func fastXORBytes(dst, a, b []byte, n int) {
-	w := n / wordSize
-	if w > 0 {
-		wordBytes := w * wordSize
-		fastXORWords(dst[:wordBytes], a[:wordBytes], b[:wordBytes])
-	}
-	for i := n - n%wordSize; i < n; i++ {
-		dst[i] = a[i] ^ b[i]
-	}
-}
-
-const wordSize = int(unsafe.Sizeof(uintptr(0)))
-const supportsUnaligned = runtime.GOARCH == "386" || runtime.GOARCH == "ppc64" || runtime.GOARCH == "ppc64le" || runtime.GOARCH == "s390x"
-
-// xor the bytes in a and b. The destination is assumed to have enough space.
-func bytesNoSIMD(dst, a, b []byte, size int) {
 	if supportsUnaligned {
-		fastXORBytes(dst, a, b, size)
+		fastXORBytes(dst, src, len(dst))
 	} else {
 		// TODO(hanwen): if (dst, a, b) have common alignment
 		// we could still try fastXORBytes. It is not clear
 		// how often this happens, and it's only worth it if
 		// the block encryption itself is hardware
 		// accelerated.
-		safeXORBytes(dst, a, b, size)
+		safeXORBytes(dst, src, len(dst))
+	}
+
+}
+
+// fastXORBytes xor in bulk. It only works on architectures that
+// support unaligned read/writes.
+func fastXORBytes(dst []byte, src [][]byte, n int) {
+	w := n / wordSize
+	if w > 0 {
+		wordBytes := w * wordSize
+
+		wordAlignSrc := make([][]byte, len(src))
+		for i := range src {
+			wordAlignSrc[i] = src[i][:wordBytes]
+		}
+		fastXORWords(dst[:wordBytes], wordAlignSrc)
+	}
+	for i := n - n%wordSize; i < n; i++ {
+		s := src[0][i]
+		for j := 1; j < len(src); j++ {
+			s ^= src[j][i]
+		}
+		dst[i] = s
 	}
 }
 
-func safeXORBytes(dst, a, b []byte, n int) {
-	ex := n % 8
-	for i := 0; i < ex; i++ {
-		dst[i] = a[i] ^ b[i]
-	}
+const wordSize = int(unsafe.Sizeof(uintptr(0)))
+const supportsUnaligned = runtime.GOARCH == "386" || runtime.GOARCH == "ppc64" || runtime.GOARCH == "ppc64le" || runtime.GOARCH == "s390x"
 
-	for i := ex; i < n; i += 8 {
-		_dst := dst[i : i+8]
-		_a := a[i : i+8]
-		_b := b[i : i+8]
-		_dst[0] = _a[0] ^ _b[0]
-		_dst[1] = _a[1] ^ _b[1]
-		_dst[2] = _a[2] ^ _b[2]
-		_dst[3] = _a[3] ^ _b[3]
-
-		_dst[4] = _a[4] ^ _b[4]
-		_dst[5] = _a[5] ^ _b[5]
-		_dst[6] = _a[6] ^ _b[6]
-		_dst[7] = _a[7] ^ _b[7]
+func safeXORBytes(dst []byte, src [][]byte, n int) {
+	for i := 0; i < n; i++ {
+		s := src[0][i]
+		for j := 1; j < len(src); j++ {
+			s ^= src[j][i]
+		}
+		dst[i] = s
 	}
 }
 
 // fastXORWords XORs multiples of 4 or 8 bytes (depending on architecture.)
 // The arguments are assumed to be of equal length.
-func fastXORWords(dst, a, b []byte) {
+func fastXORWords(dst []byte, src [][]byte) {
 	dw := *(*[]uintptr)(unsafe.Pointer(&dst))
-	aw := *(*[]uintptr)(unsafe.Pointer(&a))
-	bw := *(*[]uintptr)(unsafe.Pointer(&b))
-	n := len(b) / wordSize
-	ex := n % 8
-	for i := 0; i < ex; i++ {
-		dw[i] = aw[i] ^ bw[i]
+	sw := make([][]uintptr, len(src))
+	for i := range src {
+		sw[i] = *(*[]uintptr)(unsafe.Pointer(&src[i]))
 	}
 
-	for i := ex; i < n; i += 8 {
-		_dw := dw[i : i+8]
-		_aw := aw[i : i+8]
-		_bw := bw[i : i+8]
-		_dw[0] = _aw[0] ^ _bw[0]
-		_dw[1] = _aw[1] ^ _bw[1]
-		_dw[2] = _aw[2] ^ _bw[2]
-		_dw[3] = _aw[3] ^ _bw[3]
-		_dw[4] = _aw[4] ^ _bw[4]
-		_dw[5] = _aw[5] ^ _bw[5]
-		_dw[6] = _aw[6] ^ _bw[6]
-		_dw[7] = _aw[7] ^ _bw[7]
+	n := len(dst) / wordSize
+	for i := 0; i < n; i++ {
+		s := sw[0][i]
+		for j := 1; j < len(sw); j++ {
+			s ^= sw[j][i]
+		}
+		dw[i] = s
 	}
 }
