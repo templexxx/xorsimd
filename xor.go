f5b537426d090ff0b9461c3df04e3142a1537a92
templexxx <lucas1x1x@gmail.com>
2019-12-06 18:39:15 +0800

    Feature: Add Replace function

--- xor.go
+++ xor.go
@@ -5,7 +5,7 @@ import "github.com/templexxx/cpu"
 // AVX512 may slow down CPU Clock (maybe not).
 // TODO need more research:
 // https://lemire.me/blog/2018/04/19/by-how-much-does-avx-512-slow-down-your-cpu-a-first-experiment/
-var EnableAVX512 = false
+var EnableAVX512 = true
 
 // Encode encodes elements from source slice into a
 // destination slice. The source and destination may overlap.
@@ -27,6 +27,12 @@ func Encode(dst []byte, src [][]byte) (n int) {
 	for i := range src {
 		src[i] = src[i][:n]
 	}
+
+	if len(src) == 1 {
+		copy(dst, src[0])
+		return
+	}
+
 	f := getCPUFeature()
 	encode(dst, src, f)
 	return
@@ -36,11 +42,12 @@ const (
 	avx512 = iota
 	avx2
 	sse2
+	generic
 )
 
 // TODO: add arm feature...
 func getCPUFeature() int {
-	if useAVX512() {
+	if hasAVX512 && EnableAVX512 {
 		return avx512
 	} else if cpu.X86.HasAVX2 {
 		return avx2
@@ -49,31 +56,40 @@ func getCPUFeature() int {
 	}
 }
 
-func useAVX512() (ok bool) {
-	if !cpu.X86.HasAVX512VL {
-		return
-	}
-	if !cpu.X86.HasAVX512BW {
-		return
-	}
-	if !cpu.X86.HasAVX512F {
-		return
-	}
-	if !cpu.X86.HasAVX512DQ {
-		return
-	}
-	if !EnableAVX512 {
-		return
-	}
-	return true
-}
+var hasAVX512 = cpu.X86.HasAVX512VL &&
+	cpu.X86.HasAVX512BW &&
+	cpu.X86.HasAVX512F &&
+	cpu.X86.HasAVX512DQ
 
 // if size > nonTmpSize, it will use Non-Temporal Hint store
 const nonTmpSize = 8 * 1024 // depends on CPU Cache Size
 
-// Update parity when one data vect change
-func Update(oldData, newData, parity []byte) {
-	tmpVects := make([][]byte, 3)
-	tmpVects[0], tmpVects[1], tmpVects[2] = oldData, newData, parity
-	Encode(parity, tmpVects)
+// Update updates parity when there is one data vector being changed.
+// Returns the minimum length of oldData and newData and parity.
+// e.g.
+// oldData ⊕ A = parity
+// Pass oldData, newData, parity as args,
+// then the parity will be:
+// parity = newData ⊕ A
+func Update(oldData, newData, parity []byte) int {
+	src := make([][]byte, 3)
+	src[0], src[1], src[2] = oldData, newData, parity
+	return Encode(parity, src)
+}
+
+// Replace replaces oldData vectors with 0 or replaces 0 with newData vectors.
+// Returns the minimum length of data[i] and parity.
+//
+// It's used in two situations:
+// 1. We didn't have enough data for filling in a stripe, but still did xor encode,
+// we need replace several zero vectors with new vectors which have data after we get enough data finally.
+// 2. After compact, we may have several useless vectors in a stripe,
+// we need replaces these useless vectors with zero vectors for free space.
+func Replace(parity []byte, data [][]byte) int {
+	vects := make([][]byte, len(data)+1)
+	vects[0] = parity
+	for i := range data {
+		vects[i+1] = data[i]
+	}
+	return Encode(parity, vects)
 }
--- xor_test.go
+++ xor_test.go
@@ -2,103 +2,194 @@ package xor
 
 import (
 	"bytes"
-	crand "crypto/rand"
 	"fmt"
-	"io"
+	"math/rand"
+	"runtime"
 	"testing"
+	"time"
 
 	"github.com/templexxx/cpu"
 )
 
 const (
-	kb          = 1024
-	mb          = 1024 * 1024
-	testDataCnt = 3 // len(data)
-	// it will cover Non-Temporal Hint & loop128b, loop64b, loop_8b, loop_1b(in xor_amd64.s)
-	verifySize = nonTmpSize + 256 + 16 + 8 + 7
+	kb = 1024
+	mb = 1024 * 1024
+
+	// testMaxVects is the maximum length of source vectors,
+	// in test process, we will pick up a vects cnt randomly from [1, testMaxVects].
+	testMaxVects = 17
+	testDataCnt  = 3 // len(data)
+	// testSize is a single vector's size.
+	// it will cover Non-Temporal Hint & loop256b, loop128b, loop64b, loop_8b, loop_1b(in *_amd64.s)
+	testSize = nonTmpSize + 256 + 16 + 8 + 7
 )
 
 func TestVerifyEncode(t *testing.T) {
-	if useAVX512() {
-		verifyEncode(t, testDataCnt, avx512)
-		verifyEncode(t, testDataCnt, avx2)
-		verifyEncode(t, testDataCnt, sse2)
-	}
-	if cpu.X86.HasAVX2 {
-		verifyEncode(t, testDataCnt, avx2)
-		verifyEncode(t, testDataCnt, sse2)
-	} else {
-		verifyEncode(t, testDataCnt, sse2)
+	switch runtime.GOARCH {
+
+	case `amd64`:
+		verifyEncode(t, sse2)
+		if cpu.X86.HasAVX2 {
+			verifyEncode(t, avx2)
+			if hasAVX512 {
+				verifyEncode(t, avx512)
+			}
+		}
+
+	default:
+		verifyEncode(t, generic)
 	}
 }
 
-func verifyEncode(t *testing.T, dataCnt int, cpuFeature int) {
-	for size := 1; size <= verifySize; size++ {
-		expect := make([]byte, size)
-		result := make([]byte, size)
-		src := make([][]byte, dataCnt)
-		for j := 0; j < dataCnt; j++ {
+func verifyEncode(t *testing.T, feature int) {
+	rand.Seed(time.Now().UnixNano())
+
+	for size := 1; size <= testSize; size++ {
+		exp := make([]byte, size)
+		vcnt := randIntn(testMaxVects, 2)
+		src := make([][]byte, vcnt)
+		for j := 0; j < vcnt; j++ {
 			src[j] = make([]byte, size)
-			err := fillRandom(src[j])
-			if err != nil {
-				t.Fatal(err)
-			}
+			fillRandom(src[j])
 		}
-		for j := 0; j < size; j++ {
-			expect[j] = src[0][j] ^ src[1][j]
-		}
-		for j := 2; j < dataCnt; j++ {
-			for k := 0; k < size; k++ {
-				expect[k] ^= src[j][k]
+		for i := 0; i < size; i++ {
+			s := src[0][i]
+			for j := 1; j < vcnt; j++ {
+				s ^= src[j][i]
 			}
+			exp[i] = s
 		}
 
-		var cpuStr string
-		switch cpuFeature {
-		case avx512:
-			cpuStr = "avx512"
-		case avx2:
-			cpuStr = "avx2"
-		case sse2:
-			cpuStr = "sse2"
-		}
-		encode(result, src, cpuFeature)
-		if !bytes.Equal(expect, result) {
-			t.Fatalf("encode mismatch; size: %d; ext: %s", size, cpuStr)
+		fs := featureToString(feature)
+		act := make([]byte, size)
+		encode(act, src, feature)
+		if !bytes.Equal(exp, act) {
+			t.Fatalf("encode mismatch; vect cnt: %d, size: %d; ext: %s", vcnt, size, fs)
 		}
 	}
+}
+
+// randIntn returns, as an int, a non-negative pseudo-random number in [min,n)
+// from the default Source.
+func randIntn(n, min int) int {
+	m := rand.Intn(n)
+	if m < min {
+		m = min
+	}
+	return m
+}
+
+func featureToString(f int) (s string) {
+	switch f {
+	case avx512:
+		s = "avx512"
+	case avx2:
+		s = "avx2"
+	case sse2:
+		s = "sse2"
+	}
+	return s
 }
 
 func TestVerifyUpdate(t *testing.T) {
-	verifyUpdate(t, testDataCnt)
-}
+	rand.Seed(time.Now().UnixNano())
 
-func verifyUpdate(t *testing.T, dataCnt int) {
-	for size := 1; size <= verifySize; size++ {
-		expect := make([]byte, size)
-		result := make([]byte, size)
-		src0 := make([][]byte, dataCnt)
-		src1 := make([][]byte, dataCnt)
-		for j := 0; j < dataCnt; j++ {
+	for size := 1; size <= testSize; size++ {
+		vcnt := randIntn(testMaxVects, 2)
+		src0 := make([][]byte, vcnt)
+		src1 := make([][]byte, vcnt)
+		for j := 0; j < vcnt; j++ {
 			src0[j] = make([]byte, size)
-			err := fillRandom(src0[j])
-			if err != nil {
-				t.Fatal(err)
-			}
+			fillRandom(src0[j])
 			src1[j] = make([]byte, size)
 			copy(src1[j], src0[j])
 		}
-		Encode(result, src0)
+
+		act := make([]byte, size)
+		Encode(act, src0)
+
 		newData := make([]byte, size)
-		err := fillRandom(newData)
-		if err != nil {
-			t.Fatal(err)
+		fillRandom(newData)
+		idx := rand.Intn(vcnt)
+		Update(src0[idx], newData, act)
+
+		src1[idx] = newData
+		exp := make([]byte, size)
+		Encode(exp, src1)
+		if !bytes.Equal(exp, act) {
+			t.Fatalf("update mismatch; vect cnt: %d, size: %d", vcnt, size)
 		}
-		Update(src0[0], newData, result)
-		src1[0] = newData
-		Encode(expect, src1)
-		if !bytes.Equal(expect, result) {
-			t.Fatal("update mismatch")
+	}
+}
+
+func TestVerifyReplace(t *testing.T) {
+	verifyReplaceZero(t)
+	verifyReplaceData(t)
+}
+
+// zero vectors -> data vectors.
+func verifyReplaceZero(t *testing.T) {
+	rand.Seed(time.Now().UnixNano())
+
+	for size := 1; size <= testSize; size++ {
+		vcnt := randIntn(testMaxVects, 2)
+		zeroV := randIntn(vcnt, 1)
+		src := make([][]byte, vcnt)
+		for i := range src {
+			src[i] = make([]byte, size)
+			if i >= zeroV {
+				fillRandom(src[i])
+			}
+		}
+		act := make([]byte, size)
+		Encode(act, src)
+		data := make([][]byte, zeroV)
+		for i := range data {
+			data[i] = make([]byte, size)
+			fillRandom(data[i])
+		}
+		Replace(act, data)
+
+		exp := make([]byte, size)
+		for i := range data {
+			src[i] = data[i]
+		}
+		Encode(exp, src)
+
+		if !bytes.Equal(act, exp) {
+			t.Fatalf("replace zero mismatch; vect cnt: %d, zero cnt: %d, size: %d", vcnt, zeroV, size)
+		}
+	}
+}
+
+// data vectors -> zero vectors.
+func verifyReplaceData(t *testing.T) {
+	rand.Seed(time.Now().UnixNano())
+
+	for size := 1; size <= testSize; size++ {
+		vcnt := randIntn(testMaxVects, 2)
+		zeroV := randIntn(vcnt, 1)
+		src := make([][]byte, vcnt)
+		for i := range src {
+			src[i] = make([]byte, size)
+			fillRandom(src[i])
+		}
+		act := make([]byte, size)
+		Encode(act, src)
+		data := make([][]byte, zeroV)
+		for i := range data {
+			data[i] = src[i]
+		}
+		Replace(act, data)
+
+		exp := make([]byte, size)
+		for i := range data {
+			src[i] = make([]byte, size)
+		}
+		Encode(exp, src)
+
+		if !bytes.Equal(act, exp) {
+			t.Fatalf("replace data mismatch; vect cnt: %d, zero cnt: %d, size: %d", vcnt, zeroV, size)
 		}
 	}
 }
@@ -123,10 +214,7 @@ func benchEnc(b *testing.B, dataCnt, size int) {
 	data := make([][]byte, dataCnt)
 	for i := 0; i < dataCnt; i++ {
 		data[i] = make([]byte, size)
-		err := fillRandom(data[i])
-		if err != nil {
-			b.Fatal(err)
-		}
+		fillRandom(data[i])
 	}
 	Encode(parity, data)
 	b.SetBytes(int64(dataCnt * size))
@@ -136,7 +224,14 @@ func benchEnc(b *testing.B, dataCnt, size int) {
 	}
 }
 
-func fillRandom(p []byte) (err error) {
-	_, err = io.ReadFull(crand.Reader, p)
-	return
+// Copy from https://github.com/klauspost/reedsolomon/blob/master/reedsolomon_test.go
+// Thank him for his contribution.
+func fillRandom(p []byte) {
+	for i := 0; i < len(p); i += 7 {
+		val := rand.Int63()
+		for j := 0; i+j < len(p) && j < 7; j++ {
+			p[i+j] = byte(val)
+			val >>= 8
+		}
+	}
 }
