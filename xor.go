fad0ce0953ada005f2a2a03fc75dc45fce0c3ed4
templexxx <lucas1x1x@gmail.com>
2019-12-06 22:21:27 +0800

    Add License

--- xor.go
+++ xor.go
@@ -1,3 +1,10 @@
+/*
+ * Copyright (c) 2019. Temple3x (temple3x@gmail.com)
+ *
+ * Use of this source code is governed by the MIT License
+ * that can be found in the LICENSE file.
+ */
+ 
 package xor
 
 import "github.com/templexxx/cpu"
